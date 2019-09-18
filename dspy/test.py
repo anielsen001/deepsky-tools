a60e004c61877a18f4b3cb6a37b0c76ccecf849e
Aaron Nielsen <apn@apnielsen.com>
2019-09-18 06:14:53 -0400

    added use of memory managment methods from stacker.py

--- dspy/test.py
+++ dspy/test.py
@@ -19,17 +19,19 @@ if True:
     flat_list = glob.glob( '/home/apn/data/stars/flats-2019-06-01/iso800/Flats*.dng' )
     light_list = glob.glob( '/home/apn/data/stars/lights-2019-06-02/ursa_major/Lights*.dng' )
 
-bias_stacker = BiasStacker( bias_list[1:10] )
-bias_stacker.write('test/bias_test.fits')
+bias_stacker = BiasStacker( bias_list )
+bias_stacker.write('test/bias_test.fits', overwrite = True)
+bias_stacker.del_raw_frames()
 
 dark_stacker = DarkStacker( dark_list,
                             bias_stack = bias_stacker )
-dark_stacker.write('test/dark_test.fits')
+dark_stacker.write('test/dark_test.fits', overwrite = True)
+dark_stacker.del_raw_frames()
 
 flat_stacker = FlatStacker( flat_list ,
                             bias_stack = bias_stacker,
                             dark_stack = dark_stacker )
-flat_stacker.write('test/flat_test.fits')
+flat_stacker.write('test/flat_test.fits', overwrite = True)
 
 light_stacker = LightStacker( light_list[0:10] ,
                               bias_stack = bias_stacker,
