85b51fcecabce87d4cbbb01b3802bc0df19dfbbb
Aaron Nielsen <apn@apnielsen.com>
2019-09-18 21:19:32 -0400

    added write json to test script

--- dspy/test.py
+++ dspy/test.py
@@ -19,22 +19,25 @@ if True:
     flat_list = glob.glob( '/home/apn/data/stars/flats-2019-06-01/iso800/Flats*.dng' )
     light_list = glob.glob( '/home/apn/data/stars/lights-2019-06-02/ursa_major/Lights*.dng' )
 
-bias_stacker = BiasStacker( bias_list )
+bias_stacker = BiasStacker( bias_list[0:2] )
 bias_stacker.write('test/bias_test.fits', overwrite = True)
+bias_stacker.write_meta_json('test/bias_test.json')
 bias_stacker.del_raw_frames()
 
-dark_stacker = DarkStacker( dark_list,
+dark_stacker = DarkStacker( dark_list[0:2],
                             bias_stack = bias_stacker )
 dark_stacker.write('test/dark_test.fits', overwrite = True)
+dark_stacker.write_meta_json('test/dark_test.json')
 dark_stacker.del_raw_frames()
 
-flat_stacker = FlatStacker( flat_list ,
+flat_stacker = FlatStacker( flat_list[0:2] ,
                             bias_stack = bias_stacker,
                             dark_stack = dark_stacker )
 flat_stacker.write('test/flat_test.fits', overwrite = True)
+flat_stacker.write_meta_json('test/flat_test.json')
 flat_stacker.del_raw_frames()
 
-light_stacker = LightStacker( light_list[0:10] ,
+light_stacker = LightStacker( light_list[0:2] ,
                               bias_stack = bias_stacker,
                               dark_stack = dark_stacker,
                               flat_stack = flat_stacker,
